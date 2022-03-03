12187da9e86d968861de85b9202b0b8241628802
laurabps <laura.contact.business@gmail.com>
2022-03-03 00:10:02 -0600

    pantalla principal activity test

---
+++ app/src/main/java/com/example/llegabien/pantallaPrincipalActivity.java
@@ -0,0 +1,13 @@
+package com.example.llegabien;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pantallaPrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_principal);
+    }
+}
